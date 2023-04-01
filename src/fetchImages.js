48de283f7a3c5f94b01429dcb9e3aac5b0c1fd77
potocki92 <mateusz.potocki92@gmail.com>
2023-04-01 23:59:34 +0200

    Updated

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -1,11 +1,23 @@
-const fetchImages = name => {
-  const url = `https://pixabay.com/api/?key=34880786-eb7cfd58b108d519b70562252&q=${name}&image_type=photo`;
+import Notiflix from 'notiflix';
+
+const fetchImages = (name, page = 1, perPage = 40) => {
+  const url = `https://pixabay.com/api/?key=34880786-eb7cfd58b108d519b70562252&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
+  Notiflix.Notify.info(`Fetching images for "${name}"...`);
   return fetch(url)
     .then(response => {
       if (!response.ok) {
+        if (response.status === 429) {
+          // Too Many Requests (Zbyt wiele żądań)
+          Notiflix.Notify.warning('Too many requests. Please try again later.');
+        }
         return Promise.reject(new Error(response.status));
       }
       return response.json();
+    })
+    .then(data => {
+      const totalHits = data.totalHits;
+      Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+      return data;
     })
     .catch(error => {
       console.error(error);
--- src/index.js
+++ src/index.js
@@ -1,24 +1,140 @@
-import axios from 'axios';
+import './css/styles.css';
 
 import { fetchImages } from './fetchImages';
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const searchForm = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
 
-const response = fetchImages('cat');
+let currentPage = 1;
+let currentQuery = '';
+
+// Inicjalizacja SimpleLightbox
+let lightbox = new SimpleLightbox('.photo-card a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 searchForm.addEventListener('submit', event => {
   event.preventDefault();
-  const searchQuery = event.target.elements.searchQuery.value;
+  const searchQuery = event.target.elements.searchQuery.value.trim();
+  if (!searchQuery) {
+    return;
+  }
+
+  currentPage = 1; // reset current page
+  currentQuery = searchQuery;
+  gallery.innerHTML = ''; // Usunięcie poprzedniego wyniku wyszukiwania
   fetchImages(searchQuery)
     .then(data => {
-      console.log(data);
+      if (data.hits.length === 0) {
+        throw new Error('No images found');
+      }
+      renderList(data.hits);
+      if (data.totalHits > currentPage * 40) {
+        observer.observe(loadMoreBtn);
+      }
     })
     .catch(error => {
+      console.error(error);
       Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again'
+        'Sorry, there are no images matching your search query. Please try again.'
       );
     });
 });
+
+// loadMoreBtn.addEventListener('click', () => {
+//   currentPage += 1;
+//   fetchImages(currentQuery, currentPage)
+//     .then(data => {
+//       renderList(data.hits);
+//       gallery.scrollIntoView({
+//         behavior: 'smooth',
+//         block: 'end',
+//       });
+//       if (data.totalHits <= currentPage * 40) {
+//         loadMoreBtn.classList.add('is-hidden');
+//         Notiflix.Notify.info(
+//           "We're sorry, but you've reached the end of search results."
+//         );
+//       }
+//     })
+//     .catch(error => {
+//       Notiflix.Notify.failure(
+//         'Sorry, there was an error while fetching images. Please try again'
+//       );
+//     });
+// });
+
+const renderList = data => {
+  const markup = data
+    .map(
+      item =>
+        `<div class="photo-card"> 
+        <a href="${item.largeImageURL}">
+      <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy"/> 
+      <div class="info"> 
+        <p class="info-item"> 
+          <b>Likes:</b> ${item.likes} 
+        </p> 
+        <p class="info-item"> 
+          <b>Views:</b> ${item.views} 
+        </p> 
+        <p class="info-item"> 
+          <b>Comments:</b> ${item.comments} 
+        </p> 
+        <p class="info-item"> 
+          <b>Downloads:</b> ${item.downloads} 
+        </p> 
+      </div> 
+  </div>`
+    )
+    .join('');
+
+  gallery.insertAdjacentHTML('beforeend', markup);
+
+  // Odświeżenie SimpleLightbox dla nowych elementów
+  lightbox.refresh();
+
+  // Płynne przewijanie strony
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+};
+const options = {
+  rootMargin: '0px',
+  threshold: 0.5,
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      currentPage += 1;
+      fetchImages(currentQuery, currentPage)
+        .then(data => {
+          renderList(data.hits);
+          if (data.totalHits <= currentPage * 40) {
+            loadMoreBtn.classList.add('is-hidden');
+            Notiflix.Notify.info(
+              "We're sorry, but you've reached the end of search results."
+            );
+            observer.unobserve(entry.target);
+          }
+        })
+        .catch(error => {
+          Notiflix.Notify.failure(
+            'Sorry, there was an error while fetching images. Please try again'
+          );
+        });
+    }
+  });
+}, options);
